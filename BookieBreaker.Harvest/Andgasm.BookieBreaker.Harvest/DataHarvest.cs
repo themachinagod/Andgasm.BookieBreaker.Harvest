b405d85a15df8f1d2b01fc8f5df5ed978e4b4055
Danny Brown <machinagod@googlemail.com>
2018-10-14 16:46:19 +0100

    cleanup

--- BookieBreaker.Harvest/Andgasm.BookieBreaker.Harvest/DataHarvest.cs
+++ BookieBreaker.Harvest/Andgasm.BookieBreaker.Harvest/DataHarvest.cs
@@ -1,5 +1,4 @@
-﻿using Andgasm.BookieBreaker.RequestManager.Core;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -12,8 +11,7 @@ namespace Andgasm.BookieBreaker.Harvest
         #endregion
 
         #region Properties
-        public IRequestManager _requestmanager { get; set; }
-        //public SystemConfig _config { get; set; }
+        public HarvestRequestManager _requestmanager { get; set; }
         public ParallelOptions _po { get; set; }
         public TimeSpan ElapsedTime
         {
@@ -28,7 +26,6 @@ namespace Andgasm.BookieBreaker.Harvest
 
         public DataHarvest(int? maxthreads = null)
         {
-            //CookieString = "_gat_subdomainTracker=1; _gat=1; _gid=GA1.2.1241310394.1532547615; _ga=GA1.2.638389433.1532114109; visid_incap_774904=2VHWR4OrQJ6JcpSUOUIzzJ40UlsAAAAAVUIPAAAAAACAaMiFAU1lv1DEH1MajB012/jYOqYUG7V9; incap_ses_151_774904=hVSpROahlXZQlHG+J3cYAjEoW1sAAAAAXSx3Py7vJ+wXtAkT3UU/yQ==";
             _po = new ParallelOptions();
             _po.MaxDegreeOfParallelism = maxthreads.HasValue ? maxthreads.Value : Environment.ProcessorCount;
         }
@@ -37,7 +34,6 @@ namespace Andgasm.BookieBreaker.Harvest
         {
             if (_requestmanager == null) return false;
             if (_po == null) return false;
-            //if (_config == null) return false;
             return true;
         }
 
